fcf06802253263e511169e1147a2aff59fa1ad49
Mutiara Noor Fauzia <143862669+moetzi@users.noreply.github.com>
2023-10-20 17:11:16 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Toggle icon in the navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.mut-navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            // Active navbar links
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar when clicking navbar link (scroll)
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+};
